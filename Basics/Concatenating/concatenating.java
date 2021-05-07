8b7f14756b423e6c8f9c9c6127a51cabf216d2ca
david-torres-by <davidtorresc8@gmail.com>
2021-05-06 19:52:08 -0500

    How to concatenate in Java

---
+++ Basics/Concatenating/concatenating.java
@@ -0,0 +1,7 @@
+public class concatenating {
+    public static void main(String[] args) {
+        String name = "David Torres ";
+
+        System.out.println("Hi, I'm " + name + "Nice to meet you");
+    }
+}
