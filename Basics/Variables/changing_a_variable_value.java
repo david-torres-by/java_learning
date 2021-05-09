2d78e5af02100c28f7be3f8c16eb7f33c2d02685
david-torres-by <davidtorresc8@gmail.com>
2021-05-09 12:57:36 -0500

    Changing a variable value

---
+++ Basics/Variables/changing_a_variable_value.java
@@ -0,0 +1,9 @@
+public class changing_a_variable_value {
+    public static void main(String[] args) {
+        int number = 123;
+        System.out.println("The value of the variable is " + number);
+
+        number = 42;
+        System.out.println("The value of the variable is " + number);
+    }
+}
