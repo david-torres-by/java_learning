b2eeb7c18b97dc47a5ba4cbb1315bc272d5af29d
david-torres-by <davidtorresc8@gmail.com>
2021-05-09 17:15:48 -0500

    How to use variables

--- Basics/Variables/how_to_use_variables.java
+++ Basics/Variables/how_to_use_variables.java
@@ -1,2 +1,15 @@
-package com.java_learning.Basics.Variables;public class how_to_use_variables {
+import java.util.Scanner;
+
+public class how_to_use_variables {
+    public static void main(String[] args) {
+        String text = "This contains text";
+        int wholeNumber = 123;
+        double floatingPoint = 3.1415;
+        boolean trueorFalse = true;
+
+        System.out.println("Text Variable: " + text);
+        System.out.println("Integer variable: " + wholeNumber);
+        System.out.println("Floating-point variable: " + floatingPoint);
+        System.out.println("Boolean: " + trueorFalse);
+    }
 }
