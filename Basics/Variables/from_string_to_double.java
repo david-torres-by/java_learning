6786a309fc24c291e8f8356e009e9ffb887bd1b9
david-torres-by <davidtorresc8@gmail.com>
2021-05-10 16:19:53 -0500

    Converting from string to double

---
+++ Basics/Variables/from_string_to_double.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class from_string_to_double {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a value ");
+        double value = Double.valueOf(scanner.nextLine());
+        System.out.println("You wrote " + value);
+    }
+}
