dae95eb3e0481aaa1e6c2246723e033edf744994
david-torres-by <davidtorresc8@gmail.com>
2021-05-10 16:19:22 -0500

    Basic mathematical operations

---
+++ Basics/Calculating with numbers/basic_mathematical_operations.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class basic_mathematical_operations{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int first = 2;
+        System.out.println(first);
+        int second = 4;
+        System.out.println(second);
+
+        int sum = first + second;
+        System.out.println(sum);
+    }
+}
